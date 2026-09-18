80d7e94c652edccc50bc364103a6efc03a4cd8af
Student <Student@LabVM2049939.hxqykrmbb51uffpgidibjjt30h.jnbx.internal.cloudapp.net>
2026-09-18 15:18:51 +0200

    Added username and password corrections

--- src/prog5121poep/PROG5121POEp.java
+++ src/prog5121poep/PROG5121POEp.java
@@ -1,4 +1,3 @@
-
 package prog5121poep;
 
 import java.util.Scanner;
@@ -11,9 +10,18 @@ public class PROG5121POEp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        String firstName;
+        String lastName;
         String username;
         String password;
         String cellPhone;
+        
+        
+        System.out.print("Enter your first name: ");
+        firstName = scanner.nextLine();
+
+        System.out.print("Enter your last name: ");
+        lastName = scanner.nextLine();
         
         System.out.println("*****************");
         System.out.println("  REGISTRATION   ");
@@ -47,14 +55,61 @@ public class PROG5121POEp {
                 System.out.print("Enter cell phone number (+27...): ");
                 cellPhone = scanner.nextLine();
           
-                // Checks if user inputed the correct digits
+            // Checks if user inputed the correct digits
         if (!checkcellphonenumber(cellPhone)) {
                     System.out.println("Cell phone number incorrectly formatted or does not contain international code.");
                 } else {
-                    System.out.println("Cell phone number successfully added.");    
+                    System.out.println("Cell phone number successfully added.");  
                     
- 
+                    
+                    
+                    
+            // LOGIN PROCESS
+        System.out.println("*****************");
+        System.out.println("  REGISTRATION   ");
+        System.out.println("*****************");
+        
+        
+             System.out.print("Enter username: ");
+                    String loginUser = scanner.nextLine();       
+
+             System.out.print("Enter password: ");
+                    String loginPass = scanner.nextLine();
+        
+                    boolean isAuthenticated = loginuser(loginUser, loginPass, username, password);
+                    System.out.println(returnloginstatus(isAuthenticated, firstName, lastName));
+                }
+            }
        
     }
-    
+    }}
+     //Makes sure that there is a underscore and its less than 5 for the username
+      public static boolean checkusername(String username) {
+        return username != null && username.contains("_") && username.length() <= 5;
+    }
+      
+      //Checks that password has capital letter, special characters and 8 words
+  public static boolean checkpasswordcomplexity(String password) {
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+          boolean hasCapital = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+
+        String specialCharacters = "!@#$%^&*()_+-=[]{}|;:'\",.<>?/";
+
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                hasCapital = true;
+            } else if (Character.isDigit(ch)) {
+                hasDigit = true;
+            } else if(specialCharacters.contains(String.valueOf(ch))) {
+                hasSpecial = true;
+            }
+        }
+
+        return hasCapital && hasDigit && hasSpecial;
     }
